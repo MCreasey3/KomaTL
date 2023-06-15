2a5f6c51a947ebe743dcebf0b400fa1d8024786b
Matt Creasey <mcreasey3@outlook.com>
2023-06-15 13:39:17 -0400

    Grayscale functionality verified

--- main.py
+++ main.py
@@ -2,6 +2,18 @@ import os
 import cv2
 
 from load_image import load_image
+from preprocess import preprocess_image
 
+
+# load image for preprocessing -> text extraction -> translation
 image_path = "inputImages/input.jpg"
 image = load_image(image_path)
+
+preprocessed_image = preprocess_image(image)
+
+# Establish output directory
+output_path = "OutputImages/grayscale.jpg"
+
+# Save grayscaled image
+cv2.imwrite(output_path, preprocessed_image)
+print("Grayscaled image saved, please verify.")
---
+++ preprocess.py
@@ -0,0 +1,11 @@
+import cv2
+import os
+
+# Many ways to preprocess images for extraction and translation
+# I am not sure which methods will need to be used yet
+
+
+# Simple grayscale conversion for test
+def preprocess_image(image):
+    gray_preprocess = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    return gray_preprocess
