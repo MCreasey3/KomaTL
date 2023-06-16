00687c8826c007d510602434e634ed279f1fa503
Matt Creasey <mcreasey3@outlook.com>
2023-06-16 13:26:56 -0400

    Add .gitkeep for visibility

--- preprocess.py
+++ preprocess.py
@@ -5,7 +5,7 @@ import os
 # I am not sure which methods will need to be used yet
 
 
-# Simple grayscale conversion for test
+# Grayscale conversion
 def preprocess_image(image):
     gray_preprocess = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     return gray_preprocess
