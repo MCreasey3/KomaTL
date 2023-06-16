a89370c42cafc7ad821e1c4154dd0fb39cb1ab2a
Matt Creasey <mcreasey3@outlook.com>
2023-06-16 15:43:36 -0400

    Skew correction veified

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,5 @@
 import cv2
-import os
+# import os
 import numpy
 
 # We begin with converting the image to grayscale, and then performing Otsu's Binarization
@@ -14,4 +14,25 @@ def preprocess_image(image):
     
     # Otsu's Binarization
     _, binary_preprocess = cv2.threshold(gray_preprocess, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
-    return binary_preprocess
+    # return binary_preprocess
+
+    # Skew Correction
+    coordinates = numpy.column_stack(numpy.where(binary_preprocess > 0))
+    angle = cv2.minAreaRect(coordinates)[-1]
+    if angle < -45:
+        angle = -(90 + angle)
+    else:
+        angle = -angle
+
+    # Check for > 3 degrees skew to correct
+    angle_max = 5
+    if abs(angle) < angle_max:
+        return binary_preprocess # Skip correction when unnecessary
+    
+    # No skip
+    (height, width) = binary_preprocess.shape[:2]
+    center = (width // 2, height // 2)
+    matrix = cv2.getRotationMatrix2D(center, angle, 1.0)
+    skewcorrected_image = cv2.warpAffine(binary_preprocess, matrix, (width, height), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+
+    return skewcorrected_image
