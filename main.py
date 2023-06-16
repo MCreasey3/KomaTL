92987f4de8b2d030140ba51a48f5b39279a73dca
Matt Creasey <mcreasey3@outlook.com>
2023-06-16 13:40:45 -0400

    Binarization verified

--- main.py
+++ main.py
@@ -12,8 +12,9 @@ image = load_image(image_path)
 preprocessed_image = preprocess_image(image)
 
 # Establish output directory
-output_path = "OutputImages/grayscale.jpg"
+# For now, this will be used to verify preprocessing.
+output_path = "OutputImages/preprocessed.jpg"
 
 # Save grayscaled image
 cv2.imwrite(output_path, preprocessed_image)
-print("Grayscaled image saved, please verify.")
+print("Binary image saved, please verify.")
--- preprocess.py
+++ preprocess.py
@@ -1,11 +1,17 @@
 import cv2
 import os
+import numpy
 
-# Many ways to preprocess images for extraction and translation
-# I am not sure which methods will need to be used yet
+# We begin with converting the image to grayscale, and then performing Otsu's Binarization
+# Later, we will do a skew correction and follow up with noise removal to prepare for OCR. 
 
 
-# Grayscale conversion
+# Image preprocessing steps
 def preprocess_image(image):
+    # Grayscale conversion
     gray_preprocess = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    return gray_preprocess
+    # return gray_preprocess
+    
+    # Otsu's Binarization
+    _, binary_preprocess = cv2.threshold(gray_preprocess, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+    return binary_preprocess
